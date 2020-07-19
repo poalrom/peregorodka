5d3b0124d3c404f8d08825c96cc44d50dc021086
Alex Popkov <poalrom@yandex.ru>
2020-07-19 19:55:43 +0300

    Add tooltips

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
-// document.querySelector('.calc').addEventListener((e) => {
+const tooltip = document.querySelector('.tooltip');
+const tooltipContent = tooltip.querySelector('.tooltip__content');
+let tooltipInstance;
+const showEvents = ['mouseenter', 'focus'];
+const hideEvents = ['mouseleave', 'blur'];
 
-// });
+function destroy() {
+    if (tooltipInstance) {
+        tooltipInstance.destroy();
+        tooltipInstance = null;
+    }
+}
 
-// function onCalcItemClick(event) {}
+function create(el) {
+    tooltipContent.innerHTML = el.dataset.tooltip;
+    popperInstance = Popper.createPopper(el, tooltip, {
+        placement: 'top',
+        modifiers: [{
+            name: 'offset',
+            options: {
+                offset: [0, 8],
+            },
+        }, ],
+    });
+}
+
+function show(e) {
+    tooltip.setAttribute('data-show', '');
+    create(e.target);
+}
+
+function hide() {
+    tooltip.removeAttribute('data-show');
+    destroy();
+}
+
+function initTooltip(el) {
+    const tooltipText = el.dataset.tooltip;
+    if (!tooltipText) {
+        return;
+    }
+    destroy();
+    showEvents.forEach(event => {
+        el.addEventListener(event, show);
+    });
+
+    hideEvents.forEach(event => {
+        el.addEventListener(event, hide);
+    });
+}
+
+document.querySelectorAll('[data-tooltip]').forEach(initTooltip);

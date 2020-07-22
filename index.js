53341b2c66556aa0ae4d61a61cd125f8e4677562
Alex Popkov <poalrom@yandex.ru>
2020-07-22 22:26:23 +0300

    Add mobile slider

--- index.js
+++ index.js
@@ -72,6 +72,55 @@ const orderButtons = document.querySelectorAll('.order-toggler')
 /** CONSTRUCTOR */
 const construct = document.querySelector('.calc');
 const constructTitle = construct.querySelector('.calc__title');
+const resultInputs = construct.querySelectorAll('[type=radio][name="Дверь"]');
+const resultImages = construct.querySelectorAll('.calc__result-image');
+const resultName = construct.querySelectorAll('.calc__result');
+const additionalOptions = construct.querySelectorAll('.additional-options-input');
+
+const sliders = {
+    '.calc__step_1 .glide': null,
+    '.calc__step_2-1 .glide': null,
+    '.calc__step_2-2 .glide': null,
+    '.calc__step_2-2-1 .glide': null,
+    '.calc__step_2-2-2 .glide': null,
+    '.calc__step_2-3 .glide': null,
+    '.calc__step_2-4 .glide': null,
+};
+
+function initSliders() {
+    if (window.innerWidth <= 940) {
+        construct.querySelectorAll('.calc__items').forEach(e => {
+            e.classList.remove('calc__items');
+        });
+        Object.keys(sliders).forEach(e => {
+            if (!sliders[e]) {
+                sliders[e] = new Glide(e, {
+                    type: 'carousel'
+                }).mount();
+                return;
+            }
+            const isActive = Boolean(construct.querySelector(sliders[e].replace(' .glide', '.calc__step_active')));
+            if (!isActive) {
+                sliders[e] && sliders[e].destroy();
+                sliders[e] = new Glide(e, {
+                    type: 'carousel'
+                }).mount();
+            }
+        })
+    } else {
+        Object.keys(sliders).forEach(e => {
+            if (sliders[e]) {
+                sliders[e].destroy();
+                sliders[e] = null;
+            }
+        });
+        construct.querySelectorAll('.glide__slides').forEach(e => e.classList.add('calc__items'));
+    }
+}
+
+initSliders();
+
+window.addEventListener("resize", initSliders);
 
 function handleInputClick(input) {
     const step = input.closest('.calc__items');
@@ -87,6 +136,46 @@ function handleInputClick(input) {
     containter.classList.add('calc__item_active');
 }
 
+function fillCalcResultImage() {
+    let selectedResult;
+    resultInputs.forEach(i => {
+        if (i.checked) {
+            selectedResult = i;
+        }
+    })
+    if (!selectedResult) {
+        console.warn('Selected result not found');
+        return;
+    }
+    const selectedItem = selectedResult.closest('.glide__slide');
+    if (!selectedItem) {
+        console.warn('Closest slide not found')
+        return;
+    }
+    const resultImage = selectedItem.querySelector('.calc__item-image');
+    if (!resultImage || !resultImage.alt) {
+        console.warn('Result image not found')
+        return;
+    }
+    resultImages.forEach(i => i.src = resultImage.src);
+    resultName.forEach(n => n.textContent = resultImage.alt);
+}
+
+function fillLastStep() {
+    constructTitle.textContent = 'Поздравляем!';
+    additionalOptions.forEach(i => {
+        const optionImage = construct.querySelector(`.calc__additional-option[data-name=${i.name}]`);
+        if (!optionImage) {
+            return;
+        }
+        if (i.checked) {
+            optionImage.classList.remove('calc__additional-option_hidden');
+        } else {
+            optionImage.classList.add('calc__additional-option_hidden');
+        }
+    })
+}
+
 function goToStep(currentStep, nextStepIndex) {
     const nextStepClass = '.calc__step_' + nextStepIndex;
     const nextStep = construct.querySelector(nextStepClass);
@@ -95,10 +184,12 @@ function goToStep(currentStep, nextStepIndex) {
         return;
     }
     if (nextStepIndex === '6') {
-        constructTitle.textContent = 'Поздравляем!';
+        fillLastStep();
     }
+    fillCalcResultImage();
     currentStep.classList.remove('calc__step_active');
     nextStep.classList.add('calc__step_active');
+    Object.values(sliders).forEach(slider => slider && slider.update());
 }
 
 function handleNextClick(button) {
@@ -109,11 +200,12 @@ function handleNextClick(button) {
     }
     let nextStepIndex = button.dataset.next;
     if (!nextStepIndex) {
-        const selectedItem = step.querySelector('.calc__item_active');
+        const selectedItem = step.querySelector('.calc__item_active') || step.querySelector('.glide__slide--active');
         if (!selectedItem || !selectedItem.dataset.next) {
             console.warn('Item not selected');
             return;
         }
+        selectedItem.querySelector('input').checked = true;
         nextStepIndex = selectedItem.dataset.next;
     }
     goToStep(step, nextStepIndex);
@@ -147,4 +239,4 @@ construct.addEventListener('click', (e) => {
     if (e.target.classList.contains('skip-button')) {
         return skipStep(e.target);
     }
-})
+});
